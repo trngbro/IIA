ef5559bad35ae7d73c2d37f499c62c7161aef5fa
Trần Đình Phúc <phuctran12421@gmail.com>
2023-12-31 15:01:11 +0700

    admin page --v1

--- server/api/v1/controllers/adminControllers.js
+++ server/api/v1/controllers/adminControllers.js
@@ -1,12 +1,47 @@
 const { getStylesheets, getJavascripts } = require("../configs/assets.config")
 
 const adminControllers = {
-    test: (req, res) => {
+    index: (req, res) => {
         res.render("index", {
+            stylesheets: getStylesheets('homepage'),
+            javascripts: getJavascripts('homepage')
+        })
+    },
+
+    student_list: (req, res) => {
+        res.render("student_list",{
+            stylesheets: getStylesheets('table'),
+            javascripts: getJavascripts('table')
+        })
+    },
+
+    department_list: (req, res) => {
+        res.render("department_list",{
+            stylesheets: getStylesheets('table'),
+            javascripts: getJavascripts('table')
+        })
+    },
+
+    staff_list: (req, res) => {
+        res.render("staff_list",{
+            stylesheets: getStylesheets('table'),
+            javascripts: getJavascripts('table')
+        })
+    },
+
+    staff_add: (req, res) => {
+        res.render("staff_add",{
             stylesheets: getStylesheets(),
             javascripts: getJavascripts()
         })
-    }
+    },
+
+    upload_form: (req, res) => {
+        res.render("upload_form",{
+            stylesheets: getStylesheets(),
+            javascripts: getJavascripts()
+        })
+    },
 }
 
 module.exports = adminControllers
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
-const adminControllers = require("../api/v1/controllers/adminController")
-router.get('/', adminControllers.test)
+const adminControllers = require("../api/v1/controllers/adminControllers")
+
+router.get('/', adminControllers.index)
+router.get('/students', adminControllers.student_list)
+router.get('/departments', adminControllers.department_list)
+router.get('/staff', adminControllers.staff_list)
+router.get('/staff/add', adminControllers.staff_add)
+router.get('/chatbot/upload', adminControllers.upload_form)
 
 module.exports = router;
